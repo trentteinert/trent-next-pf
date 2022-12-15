b5f4effa1e48cc7c594c1cd62612de71c882751a
trentteinert <102979377+trentteinert@users.noreply.github.com>
2022-12-15 15:52:48 -0500

    init please god

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    // limit of 25 deviceSizes values
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    // limit of 25 imageSizes values
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    // limit of 50 domains values
-    domains: [],
-
-    // ordered list of acceptable optimized image formats (mime types)
-    formats: ['image/webp'],
-    // enable dangerous use of SVG images
-    dangerouslyAllowSVG: false,
-    // set the Content-Security-Policy header
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    // limit of 50 objects
-    remotePatterns: [],
-    // when true, every image will be unoptimized
-    unoptimized: false,
-  },
+  images: {},
 };
